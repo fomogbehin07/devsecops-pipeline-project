ade8d05bc0a043771925ebd22638b7721de5f1cd
fomogbehin <fomogbehin07@gmail.com>
2026-04-24 02:11:46 -0700

    modify the main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,11 @@
-resource "aws_s3_bucket" "public_bucket" {
-  bucket = "my-public-demo-bucket"
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-secure-demo-bucket"
 }
 
-resource "aws_s3_bucket_acl" "public_acl" {
-  bucket = aws_s3_bucket.public_bucket.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket                  = aws_s3_bucket.public_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+resource "aws_s3_bucket_public_access_block" "secure_access" {
+  bucket                  = aws_s3_bucket.secure_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
